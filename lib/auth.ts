9f0f62de078cf8ad07657779c2c14b41e3bfee74
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-07-17 12:04:53 +0100

    feat: setup google OAuth

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,8 +13,23 @@ const prisma = new PrismaClient({
 });
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
+
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+
   plugins: [organization()],
 });
