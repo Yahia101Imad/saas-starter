8b73bd569042c55031089ac969ef2c22a4f21299
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-07-18 15:31:43 +0100

    feat: add logout btn

--- app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getSession } from "@/lib/session";
-
-export default async function TestPage() {
-  const session = await getSession();
-
-  return <pre>{JSON.stringify(session, null, 2)}</pre>;
-}
